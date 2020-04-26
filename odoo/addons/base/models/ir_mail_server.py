42dd1fc5f07ef50cb3c566af26c153507ad20bc8
qmo-odoo <qmo@odoo.com>
2020-04-26 13:29:52 +0200

    [ADD] {google,fetchmail}_gmail: OAuth for gmail servers
    Purpose
    =======
    Less secured apps are no longer supported by google, therefore, we need
    to transition to the OAuth2 authentication system.
    
    Specifications
    ==============
    On the external server configuration in the main settings, users will
    have to activate Gmail support.
    
    Then, on incoming / outgoing mail servers forms, they will have to
    introduce an authorization code that they will have fetched via a link
    on the form.
    
    Task 2170676

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -263,13 +263,26 @@ class IrMailServer(models.Model):
             # See also bug #597143 and python issue #5285
             smtp_user = pycompat.to_native(ustr(smtp_user))
             smtp_password = pycompat.to_native(ustr(smtp_password))
-            connection.login(smtp_user, smtp_password)
+            self._smtp_login(connection, smtp_user, smtp_password, mail_server)
 
         # Some methods of SMTP don't check whether EHLO/HELO was sent.
         # Anyway, as it may have been sent by login(), all subsequent usages should consider this command as sent.
         connection.ehlo_or_helo_if_needed()
 
         return connection
+
+    def _smtp_login(self, connection, smtp_user, smtp_password, mail_server):
+        """Authenticate the SMTP connection.
+
+        Can be overridden in other module for different authentication methods.
+
+        :param connection: The SMTP connection to authenticate
+        :param smtp_user: The user to used for the authentication
+        :param smtp_password: The password to used for the authentication
+        :param mail_server: The mail server used to retrieve the user / password.
+            Empty record if the credentials have been taken from the odoo-bin argument
+        """
+        connection.login(smtp_user, smtp_password)
 
     def build_email(self, email_from, email_to, subject, body, email_cc=None, email_bcc=None, reply_to=False,
                     attachments=None, message_id=None, references=None, object_id=False, subtype='plain', headers=None,
