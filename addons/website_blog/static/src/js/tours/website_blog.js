fd21f59ab333ca3140f282055343c4f4be3e0341
Benoit Socias <bso@odoo.com>
2021-04-23 10:15:32 +0200

    [IMP] website, website_blog: use standard background options for covers
    Before this commit cover properties had their own options for their
    background image.
    
    After this commot cover properties rely on the standard background
    options.
    
    task-2359250

--- addons/website_blog/static/src/js/tours/website_blog.js
+++ addons/website_blog/static/src/js/tours/website_blog.js
@@ -27,10 +27,10 @@ odoo.define("website_blog.tour", function (require) {
         position: "top",
         run: "text",
     }, {
-        trigger: "we-button[data-background]:nth(1)",
+        trigger: "we-button[data-toggle-bg-image]",
         extra_trigger: "#wrap div[data-oe-expression=\"blog_post.name\"]:not(:containsExact(\"\"))",
         content: Markup(_t("Set a blog post <b>cover</b>.")),
-        position: "right",
+        position: "left",
     }, {
         trigger: ".o_select_media_dialog .o_we_search",
         content: _t("Search for an image. (eg: type \"business\")"),
