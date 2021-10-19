c683907382229cc6d15b865b6ae0b0b21eb3c9d6
yhu-odoo <yhu@odoo.com>
2021-10-19 17:07:37 +0200

    [IMP] stock(_picking_batch): improve set/clear qty button
    Add a "clear qty" button to revert the change by "set qty" button in
    (batch) picking.
    On the move, Whenever the qty_done != reserved, we consider it a
    manual change from the user. "set/clear qty" buttons won't change the
    line with manual change.
    Show "set qty" button when there are moves with 0 qty_done and non-zero
    reserved. Show "clear qty" button when we don't show "set qty" button
    and there are moves with qty done == reserved but the number is not 0.
    
    Task-2659233

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1844,6 +1844,10 @@ class StockMove(models.Model):
                     continue
                 move_line.qty_done = move_line.product_uom_qty
 
+    def _clear_quantities_to_zero(self):
+        self.filtered(lambda m: m.state in ('partially_available', 'assigned') and \
+            float_compare(m.quantity_done, m.reserved_availability, precision_rounding=m.product_uom.rounding) == 0).move_line_ids.qty_done = 0
+
     def _adjust_procure_method(self):
         """ This method will try to apply the procure method MTO on some moves if
         a compatible MTO route is found. Else the procure method will be set to MTS
