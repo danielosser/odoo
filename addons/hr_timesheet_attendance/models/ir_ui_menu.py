c2b22cd025f23b6cbb1e333ee1c1c680df12e8c4
Priyanka Kakadiya <pka@odoo.com>
2021-11-10 11:30:18 +0530

    [IMP] *: improve report security * hr_timesheet_{attendance}, project, rating, sale_timesheet
    - removed the 'admin' group on the reporting menu items so that 'users' can
      access these menus.
    - 'timesheet / attendance' menu should only show if user has access for both
      the attendance and timesheet.
    - Project/User should access 'Task Analysis' report And Added rule for user.
    - customer ratings: only show ratings which has access of their project/task.
    - ratings: user should not able to edit ratings.
    
    LINKS:
    
    Task-2467564

--- addons/hr_timesheet_attendance/models/ir_ui_menu.py
+++ addons/hr_timesheet_attendance/models/ir_ui_menu.py
@@ -9,8 +9,6 @@ class IrUiMenu(models.Model):
 
     def _load_menus_blacklist(self):
         res = super()._load_menus_blacklist()
-        if self.env.user.has_group('project.group_project_stages'):
-            res.append(self.env.ref('project.menu_projects').id)
-        if self.env.user.has_group('project.group_project_user'):
-            res.append(self.env.ref('project.rating_rating_menu_project').id)
+        if not (self.env.user.has_group('hr_attendance.group_hr_attendance_user') and self.env.user.has_group('hr_timesheet.group_hr_timesheet_user')):
+            res.append(self.env.ref('hr_timesheet_attendance.menu_hr_timesheet_attendance_report').id)
         return res
