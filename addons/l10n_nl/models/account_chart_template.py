467f41b1be9680d2a6884bdfbec491148763047e
Shawcker <anpp@odoo.com>
2021-11-16 14:56:54 +0100

    [IMP] l10n_nl: update COA for the Netherlands
    The dutch COA is outdated and incomplete. Some accounts have wrong types, and some accounts are missing.
    Correct the COA so that every account is correctly categorized.
    
    Changes:
    - Add and correct accounts in the dutch COA
    - Override the creation of the 'bank suspense account' to modify its type to current assets.
    
    Task id=2674603

--- addons/l10n_nl/models/account_chart_template.py
+++ addons/l10n_nl/models/account_chart_template.py
@@ -23,3 +23,18 @@ class AccountChartTemplate(models.Model):
             res.setdefault('tag_ids', [])
             res['tag_ids'].append((4, xml_id))
         return res
+
+    @api.model
+    def _create_liquidity_journal_suspense_account(self, company, code_digits):
+        # Change account type to current assets (from current liabilities)
+        # Avoids artificially inflating actives and passives
+        if company.account_fiscal_country_id.code == 'NL':
+            return self.env['account.account'].create({
+                'name': _("Bank Suspense Account"),
+                'code': self.env['account.account']._search_new_account_code(company, code_digits, company.bank_account_code_prefix or ''),
+                'user_type_id': self.env.ref('account.data_account_type_current_assets').id,
+                'company_id': company.id,
+                'tag_ids': [self.env.ref('l10n_nl.account_tag_25').id],
+            })
+        else:
+            return super(AccountChartTemplate, self)._create_liquidity_journal_suspense_account(company, code_digits)
