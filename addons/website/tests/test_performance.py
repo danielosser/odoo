a0a35a6ec9419996916be69188b468eeb4d5c759
Julien Castiaux <juc@odoo.com>
2021-11-19 16:07:04 +0100

    rde review, query count lower thanks to get_cached

--- addons/website/tests/test_performance.py
+++ addons/website/tests/test_performance.py
@@ -78,16 +78,16 @@ class TestWebsitePerformance(UtilPerf):
 
     def test_10_perf_sql_queries_page(self):
         # standard untracked website.page
-        self.assertEqual(self._get_url_hot_query(self.page.url), 9)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 8)
         self.menu.unlink()
-        self.assertEqual(self._get_url_hot_query(self.page.url), 11)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 10)
 
     def test_15_perf_sql_queries_page(self):
         # standard tracked website.page
         self.page.track = True
-        self.assertEqual(self._get_url_hot_query(self.page.url), 17)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 16)
         self.menu.unlink()
-        self.assertEqual(self._get_url_hot_query(self.page.url), 19)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 18)
 
     def test_20_perf_sql_queries_homepage(self):
         # homepage "/" has its own controller
@@ -96,7 +96,7 @@ class TestWebsitePerformance(UtilPerf):
     def test_30_perf_sql_queries_page_no_layout(self):
         # website.page with no call to layout templates
         self.page.arch = '<div>I am a blank page</div>'
-        self.assertEqual(self._get_url_hot_query(self.page.url), 9)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 8)
 
     def test_40_perf_sql_queries_page_multi_level_menu(self):
         # menu structure should not impact SQL requests
@@ -114,7 +114,7 @@ class TestWebsitePerformance(UtilPerf):
         menu_bb.parent_id = menu_b
         menu_aa.parent_id = menu_a
 
-        self.assertEqual(self._get_url_hot_query(self.page.url), 9)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 8)
 
     def test_50_perf_sql_web_assets(self):
         # assets route /web/assets/..
