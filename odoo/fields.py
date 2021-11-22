3001bcc42617600ab251385133ead195ff0a1d7d
Raphael Collet <rco@odoo.com>
2021-11-22 14:14:55 +0100

    [IMP] fields: make related fields editable but not invertible
    We now have the following behavior:
    
         definition                  | editable | updates target
        -----------------------------+----------+----------------
         related=...                 |    no    |    no
         related=..., readonly=False |    yes   |    no
         related=..., inverse=True   |    yes   |    yes

--- odoo/fields.py
+++ odoo/fields.py
@@ -383,7 +383,7 @@ class Field(MetaField('DummyField', (object,), {})):
             attrs['store'] = store = attrs.get('store', False)
             attrs['compute_sudo'] = attrs.get('compute_sudo', attrs.get('related_sudo', True))
             attrs['copy'] = attrs.get('copy', False)
-            attrs['readonly'] = attrs.get('readonly', True)
+            attrs['readonly'] = attrs.get('readonly', not attrs.get('inverse'))
         if attrs.get('company_dependent'):
             # by default, company-dependent fields are not stored, not computed
             # in superuser mode and not copied
@@ -538,7 +538,7 @@ class Field(MetaField('DummyField', (object,), {})):
 
         # determine dependencies, compute, inverse, and search
         self.compute = self._compute_related
-        if self.inherited or not (self.readonly or field.readonly):
+        if self.inherited or self.inverse:
             self.inverse = self._inverse_related
         if field._description_searchable:
             # allow searching on self only if the related field is searchable
