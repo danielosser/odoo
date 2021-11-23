fb005f6755acdfb6f5f4872b0c3993d1cf499966
Davan CHIEM DAO <dacd@odoo.com>
2021-11-23 14:34:03 +0100

    [ADD] l10n_rs
    Description:
    Adding new module for Serbian Accounting
    Contains COA and Taxes
    
    Task: 2697970
    
    Doc:
    Attached files to task 2352412
    https://www.apr.gov.rs/registers/financial-statements/financial-statements-forms/legal-entities-that-apply-ifrs-or-ifrs-for-smes.1707.html
    https://www.wtsserbia.com/wp-content/uploads/2019/01/NEW-Chart-of-accounts-Kontni-okvir-FINAL-Srpski-English-Deutsch.xlsx
    https://www.wtsserbia.com/en/blog-en/accounting-in-serbia/
    https://www.wtsserbia.com/en/blog-en/taxation-in-serbia-investor-s-guide/

---
+++ addons/l10n_rs/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Serbia - Accounting',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'data/account_chart_template_data.xml',
+        'data/account.account.template.csv',
+        'data/l10n_rs_chart_data.xml',
+        'data/account_tax_group_data.xml',
+        'data/account_tax_template_data.xml',
+        'data/account.group.template.csv',
+        'data/account_chart_template_configure_data.xml',
+    ],
+    'license': 'LGPL-3',
+
+}
