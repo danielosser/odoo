189e1faa13cf661f932304053142f72a0aed7b9b
Mohammed Shekha <msh@odoo.com>
2021-12-01 14:17:26 +0530

    [IMP] gstr1 report improvements

--- addons/l10n_in/static/src/js/gstr1.js
+++ addons/l10n_in/static/src/js/gstr1.js
@@ -12,41 +12,95 @@ odoo.define('l10n_in.gstr1', function (require) {
         contentTemplate: 'GSTR1',
 
         init(parent, fields) {
-            this._super(...arguments);
+            this.actionManager = parent;
             this.data = {
                 filters: {
                     date: {},
                     journal: {},
                 },
-                sections: [{
-                    columns: [
-                        { name: 'Type of Invoice' },
-                        { name: 'Count of Documents' },
-                        { name: 'Taxable Amount(A)' },
-                        { name: 'Taxable Amount(B)' },
-                        { name: 'Invoice Value', name_details: '(Total of Previous Columns May Not Match(₹)'},
-                        { button_name: 'view_edit' },
-                    ],
-                    rows: [
-                        {
-                            row_data: ['<strong>B2B</strong> <span class="text-muted">(4A, 4B, 4C, 6B, 6C)</span>', 1, 100, 10, 110, ],
-                            row_domain: [[]],
-                        }
-                    ]
-                }, {
-                    columns: [],
-                    rows: []
-                }, {
-                    columns: [],
-                    rows: []
-                }]
+                sections: {
+                    invoice_section: {
+                        columns: [
+                            { string: 'Type of Invoice' },
+                            { string: 'Count of Documents' },
+                            { string: 'Taxable Amount(A)' },
+                            { string: 'Taxable Amount(B)' },
+                            { string: 'Invoice Value', meta_string: '(Total of Previous Columns May Not Match(₹)'},
+                        ],
+                        rows: [
+                            {
+                                row_data: [
+                                    { value: '<strong>B2B</strong> <span class="text-muted">(4A, 4B, 4C, 6B, 6C)</span>', type: 'html'},
+                                    { value: 1 },
+                                    { value: 100 },
+                                    { value: 10 },
+                                    { value: 11010 }
+                                ],
+                                row_domain: [[]],
+                            }
+                        ]
+                    },
+                    order_section: {
+                        columns: [
+                            { string: 'Type of Invoice' },
+                            { string: 'Taxable Amount(A)(₹)' },
+                            { string: 'Taxable Amount(B)(₹)' },
+                            { string: 'Invoice Value', meta_string: '(Total of Previous Columns May Not Match(₹)' },
+                        ],
+                        rows: [
+                            {
+                                row_data: [
+                                    { value: '<strong>B2B Others</strong> <span class="text-muted">(7)</span>', type: 'html' },
+                                    { value: 1 },
+                                    { value: 100 },
+                                    { value: 10 },
+                                    { value: 110 }
+                                ],
+                                row_domain: [[]],
+                            }
+                        ]
+                    },
+                    amendments_section: {
+                        columns: [
+                            { string: 'Type of Invoice' },
+                            { string: 'Count of Documents' },
+                            { string: 'Taxable Amount(A)(₹)' },
+                            { string: 'Taxable Amount(B)(₹)' },
+                            { string: 'Invoice Value' },
+                        ],
+                        rows: [
+                            {
+                                row_data: [
+                                    { value: '<strong>B2B Amendments</strong> <span class="text-muted">(9A)</span>', type: 'html' },
+                                    { value: 1 },
+                                    { value: 100 },
+                                    { value: 10 },
+                                    { value: 110 }
+                                ],
+                                row_domain: [[]],
+                            }
+                        ]
+                    },
+                },
             };
+            return this._super(...arguments);
         },
-        async willStart() {
-            await this.generateSearchView();
+        willStart() {
+            const prom = this._super(...arguments);
+            return Promise.all([prom, this.generateSearchView()]);
+        },
+        start() {
+            this.controlPanelProps.cp_content = {
+                // $buttons: this.$buttons,
+                $searchview_buttons: this.$searchview_buttons,
+                // $pager: this.$pager,
+                $searchview: this.$searchview,
+            };
+            return this._super(...arguments);
         },
         generateSearchView() {
-            this.$searchview_buttons = QWeb.render("search_template", { widget: this });
+            this.$searchview_buttons = $(QWeb.render("search_template", { widget: this }));
+            return Promise.resolve();
         },
     });
 
