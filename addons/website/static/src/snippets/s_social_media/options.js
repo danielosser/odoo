297d6bcee949f3fda48fb15c9e786e34853f166b
Guillaume (gdi) <gdi@odoo.com>
2021-12-02 11:50:47 +0100

    fix small issue + string comparaison function

--- addons/website/static/src/snippets/s_social_media/options.js
+++ addons/website/static/src/snippets/s_social_media/options.js
@@ -89,41 +89,57 @@ options.registry.SocialMedia = options.Class.extend({
     /**
      * Apply the we-list on the target
      *
-     * @param  {} value array of objects {name: 'https://...', el_name: 'social_facebook', is_toggled: true, undeletable: false}
+     * @param  {} value array of objects 
+     * {name: 'https://...', el_name: 'social_facebook', is_toggled: true,
+     * undeletable: false}
      */
-    renderListItems: async function (previewMode, value, params) { // This will receive data from we-list widget and will update the target.
+    renderListItems: async function (previewMode, value, params) {
+        // This receive data from we-list widget and will update the target.
         const entries = JSON.parse(value);
         const newDBLinks = [];
-        // TODO : handle the case where an element have been removed from the list.
+        // Handle element deletation.
         const anchorsEls = this.$target[0].querySelectorAll('a[data-social-id]');
         if (entries.length < anchorsEls.length) {
-            console.log('[SocialMedia] Warning: some elements have been removed from the list.');
             const existingSocialIds = entries.map(entry => entry.el_name);
-            console.log('[SocialMedia] existingSocialIds: ', existingSocialIds);
             for (let i = 0; i < anchorsEls.length; i++) {
                 const socialId = anchorsEls[i].dataset.socialId;
                 if (!existingSocialIds.includes(socialId)) {
-                    console.log('[SocialMedia] Removing socialId: ', socialId);
                     console.log('[SocialMedia] Removing anchorEl: ', anchorsEls[i]);
+                    anchorsEls[i].remove();
+                    break;
                 }
             }
         }
-        //const anchorsEls = this.$target[0].querySelectorAll('a[data-social-id]');
-/*         if (entries.length < anchorsEls.length) {
-            const existingSocialIds = entries.map(entry => entry.el_name);
-            console.log('I have to delete', existingSocialIds);
-            for (let i = 0; i < anchorsEls.length; i++) {
-                const socialId = anchorsEls[i].dataset.socialId;
-                if (!existingSocialIds.includes(socialId)) {
-                    console.log('remove', socialId)
-                    this.$target[0].querySelector(`a[data-social-id=${socialId}]`).remove();
-                }
-            }
-        } */
         for (let i = 0; i < entries.length; i++) {
             const entry = entries[i];
+            if (!entry.el_name) {
+                break; // It happens when you move a link that you are editing.
+            }
             const anchorEl = this.$target[0].querySelector(`[data-social-id="${entry.el_name}"]`);
             if (!anchorEl) {
+                console.log('[SocialMedia] Create a new anchorEl: ', entry.el_name);
+                // Tests favicon matching.
+                {
+                console.log('*****Test****');
+                console.log('***Youtube:');
+                console.log(this.findTheMoreAccurateIcon("https://youtu.be/i8hHlexFtN0"));
+                console.log(this.findTheMoreAccurateIcon("https://www.youtube.com/channel/UCkQPikELWZFLgQNHd73jkdg"));
+                console.log(this.findTheMoreAccurateIcon("https://www.youtube.com/user/Odoo"));
+                console.log(this.findTheMoreAccurateIcon("https://www.youtube.com/watch?v=IS2tAkl4C2g"));
+                console.log('***Instagram:');
+                console.log(this.findTheMoreAccurateIcon("https://www.instagram.com/odoo.official/"));
+                console.log(this.findTheMoreAccurateIcon("https://www.instagram.com/useId?v=dQw4w9WgXcQ"));
+                console.log(this.findTheMoreAccurateIcon("https://www.istgm.com/useId?v=dQw4w9WgXcQ"));
+                console.log("***Twitter:")
+                console.log(this.findTheMoreAccurateIcon("https://www.twitter.com/useId?v=dQw4w9WgXcQ"));
+                console.log("***Linkedin:")
+                console.log(this.findTheMoreAccurateIcon("https://www.linkedin.com/posts/catherine-vieslet-1281aa14_business-analyst-odoo-activity-6871424528891830273-BMRH"));
+                console.log("***Github:")
+                console.log(this.findTheMoreAccurateIcon("https://github.com/odoo/odoo"));
+                console.log("***Facebook:")
+                console.log(this.findTheMoreAccurateIcon("https://www.facebook.com/odoo"));
+                }
+                // End tests
                 const newAnchorEl = document.createElement('a');
                 newAnchorEl.setAttribute('href', entry.display_name);
                 newAnchorEl.dataset.socialId = entry.el_name;
@@ -144,7 +160,7 @@ options.registry.SocialMedia = options.Class.extend({
                 dbData[entry.el_name] = entry.display_name;
             } else {
                 // Handle URL change for custom links.
-                // TODO search a amazing fa-icon that match the url.
+                // TODO search an amazing fa-icon that match the url.
                 anchorEl.setAttribute('href', entry.display_name);
             }
             // Place it at the correct position
@@ -168,11 +184,61 @@ options.registry.SocialMedia = options.Class.extend({
             ],
         });
     },
-    /**
-     * @override
-     */
-    _renderCustomXML: async function (uiFragment) {
-        const weListEl = uiFragment.querySelector('we-list');
+    findTheMoreAccurateIcon(url) {
+        // const icon = url.match(/^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|twitter\.com|youtube\.com|instagram\.com|linkedin\.com|github\.com)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/);
+        const supportedSocialMedia = ['facebook', 'twitter', 'youtube', 'instagram', 'linkedin', 'github'];
+        let moreAccurateSocialMedia = '';
+        let scoreOfMoreAccurateSocialMedia = 0;
+        for (let i = 0; i < supportedSocialMedia.length; i++) {
+            // TODO remove after .com/ data
+            url = url.match(/^https?:\/\/[^#?\/]+/)[0];
+            const score = this.similarity(url, `https://www.${supportedSocialMedia[i]}.com`);
+            if (score > scoreOfMoreAccurateSocialMedia) {
+                scoreOfMoreAccurateSocialMedia = score;
+                moreAccurateSocialMedia = supportedSocialMedia[i];
+            }
+        }
+        return `fa-${moreAccurateSocialMedia}`;
+    },
+    // https://en.wikipedia.org/wiki/Levenshtein_distance
+    similarity(s1, s2) {
+        let longer = s1;
+        let shorter = s2;
+        if (s1.length < s2.length) {
+            longer = s2;
+            shorter = s1;
+        }
+        const longerLength = longer.length;
+        if (longerLength === 0) {
+            return 1.0;
+        }
+        return (longerLength - this.editDistance(longer, shorter)) / parseFloat(longerLength);
+    },
+    editDistance(s1, s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        let costs = new Array();
+        for (let i = 0; i <= s1.length; i++) {
+            let lastValue = i;
+            for (let j = 0; j <= s2.length; j++) {
+                if (i === 0) {
+                    costs[j] = j;
+                } else {
+                    if (j > 0) {
+                        var newValue = costs[j - 1];
+                        if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        }
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
+            }
+            if (i > 0) {
+                costs[s2.length] = lastValue;
+            }
+        }
+        return costs[s2.length];
     },
 });
 
